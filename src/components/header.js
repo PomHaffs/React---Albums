e3350ba4f0a246064dc854f8d41b9c1c8bc8e4db
PomHaffs <tomashaffenden@icloud.com>
2017-01-12 11:59:57 +1100

    Added Header component

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+//import library
+import React from 'react';
+import { Text } from 'react-native';
+
+//Make a Component
+const Header = () => <Text>Albums!</Text>;
+
+//Make the component avalible to the rest of the App
+export default Header;
