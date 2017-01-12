4ddb1fcda2ed5f62dbcd7f4b8a5ef648d86081e7
PomHaffs <tomashaffenden@icloud.com>
2017-01-12 14:10:11 +1100

    added ablum list component

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,16 @@
 //Import a library to helpcreate Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 //get a component from elsewhere
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 //Create a compoonent
 const App = () => (
-  <Header />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 
